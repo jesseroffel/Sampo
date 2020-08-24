1e00fd1830ba06b39c4ff9531475cb3c9e217e0c
Jesse Roffel <jesseroffel@live.nl>
2020-08-24 14:18:01 +0200

    [-] Reset of project and folder structure
    Restarting the repository fresh by following a new folder structure and solution layout.

--- rakas/ballpit/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "core/engine.h"
-
-int main(char* argc, char** argv)
-{
-	Rakas::Engine* engine = Rakas::Engine::Rakas_Init(RAKAS_INIT_BASIC);
-	printf("HelloWorld!");
-	engine->Rakas_Quit();
-	return 0;
-}
