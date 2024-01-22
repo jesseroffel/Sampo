92c6b81a1416b41f71edcbfbb70c0132ab8cd008
Jesse Roffel <dev@jesseroffel.com>
2024-01-22 16:22:17 +0100

    Basic console arguments container, C++ standard upgraded to C++20

--- framework/src/sampo/core/entry_point.hpp
+++ framework/src/sampo/core/entry_point.hpp
@@ -1,18 +1,24 @@
 #pragma once
 #include "sampo/network/network_core.hpp"
 #include "sampo/network/socket_api.hpp"
+#include "sampo/core/console_arguments.hpp"
+
+void* operator new(std::size_t sz) {
+	std::cout << "Allocating: " << sz << '\n';
+	return std::malloc(sz);
+}
 
 #ifdef SAMPO_PLATFORM_WINDOWS
 	extern Sampo::Application* Sampo::CreateApplication();
 
-	int main(int argc, char** argv)
+	int main(int argc, char* argv[])
 	{
-		UNUSED(argc, argv);
-
+		Sampo::ConsoleArguments(argc, argv);
 		// Init required systems
 		Sampo::Log::Init();
 
 		Sampo::Scope<Sampo::SocketAPI> SocketApi = Sampo::SocketAPI::Create();
+
 		SocketApi->Init();
 
 		// Application
